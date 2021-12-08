9bbd14c537d981a4cf4f518011947f2fb1793721
Nguyen The Anh <nguyentheanh17071998@gmail.com>
2021-12-08 23:43:46 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          The Anh Learn React
         </a>
       </header>
     </div>
